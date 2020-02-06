38428e63b91c80b0aa7b9a29b6ba5e76fa742247
tet0 <theophile.braye@gmail.com>
2020-02-06 22:23:13 +0100

    Ajout d'une enumeration IdCarte + Class DistributeurDeCarte

--- src/models/Deck.java
+++ src/models/Deck.java
@@ -7,4 +7,16 @@ import java.util.ArrayList;
 public class Deck {
     private ArrayList<Carte> cartes;
     private int indiceDansLeDeck;
+
+    public Deck(){
+
+    }
+
+    public ArrayList<Carte> getCartes() {
+        return cartes;
+    }
+
+    public int getIndiceDansLeDeck() {
+        return indiceDansLeDeck;
+    }
 }
---
+++ src/models/cartes/DistributeurDeCarte.java
@@ -0,0 +1,74 @@
+package models.cartes;
+
+import models.cartes.types.action.*;
+import models.cartes.types.tresor.*;
+import models.cartes.types.victoire.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Class permetant de connaitre et gerer la distribution des cartes
+ * */
+public class DistributeurDeCarte {
+    /**
+     * dictionnaire de cartes
+     */
+    private static final HashMap<IdCarte, Class<? extends Carte>> dicoCards = new HashMap<IdCarte, Class<? extends Carte>>();
+
+    static {
+        dicoCards.put(IdCarte.ARTISANT, Artisan.class);
+        dicoCards.put(IdCarte.ATELIER, Atelier.class);
+        dicoCards.put(IdCarte.BRIGAND, Brigand.class);
+        dicoCards.put(IdCarte.BIBLIOTHEQUE, Bibliotheque.class);
+        dicoCards.put(IdCarte.BRACONIER, Braconnier.class);
+        dicoCards.put(IdCarte.BUREAUCRATE, Bureaucrate.class);
+        dicoCards.put(IdCarte.CAVE, Cave.class);
+        dicoCards.put(IdCarte.CHANBRE_DU_CONSEIL, ChambreDuConseil.class);
+        dicoCards.put(IdCarte.CHAPELLE, Chapelle.class);
+        dicoCards.put(IdCarte.DOUVES, Douves.class);
+        dicoCards.put(IdCarte.FESTIVAL, Festival.class);
+        dicoCards.put(IdCarte.FORGERON, Forgeron.class);
+        dicoCards.put(IdCarte.JARDIN, Jardin.class);
+        dicoCards.put(IdCarte.LABORATOIRE, Laboratoire.class);
+        dicoCards.put(IdCarte.MARCHAND, Marchand.class);
+        dicoCards.put(IdCarte.MARCHE, Marche.class);
+        dicoCards.put(IdCarte.MILICE, Millice.class);
+        dicoCards.put(IdCarte.MINE, Mine.class);
+        dicoCards.put(IdCarte.PRESAGE, Presage.class);
+        dicoCards.put(IdCarte.PRETEUR_SUR_GAGE, PreteurSurGage.class);
+        dicoCards.put(IdCarte.RENOVATION, Renovation.class);
+        dicoCards.put(IdCarte.SALLE_DU_THRONE, SalleDuTrone.class);
+        dicoCards.put(IdCarte.SENTINNELLE, Sentinelle.class);
+        dicoCards.put(IdCarte.SORCIERE, Sorciere.class);
+        dicoCards.put(IdCarte.VASSAL, Vassal.class);
+        dicoCards.put(IdCarte.VILLAGE, Village.class);
+        dicoCards.put(IdCarte.OR, Or.class);
+        dicoCards.put(IdCarte.ARGENT, Argent.class);
+        dicoCards.put(IdCarte.CUIVRE, Cuivre.class);
+        dicoCards.put(IdCarte.DOMAINE, Domaine.class);
+        dicoCards.put(IdCarte.DUCHE, Duche.class);
+        dicoCards.put(IdCarte.MALEDICTION, Malediction.class);
+        dicoCards.put(IdCarte.PROVINCE, Province.class);
+    }
+
+    /**
+     * Renvoie une liste de cartes
+     *
+     * @param number : le nombre de cartes de la liste
+     * @param idCarte : l'id de la carte à instancier (voir enum IdCarte)
+     * @return une liste de cartes de même type
+     */
+    public static ArrayList<Carte> distribue(int number, IdCarte idCarte) {
+        ArrayList stack = new ArrayList();
+        for (int i = 0; i < number; i++) {
+            try {
+                stack.add(dicoCards.get(idCarte).newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return stack;
+    }
+}
