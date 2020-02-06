5cafc3de06f3a3c759d0545fdea326541ad1fc59
Indigoblin <axolot.jvc@gmail.com>
2020-02-06 11:42:47 +0100

    Add class Carte

--- src/models/cartes/Carte.java
+++ src/models/cartes/Carte.java
@@ -1,4 +1,10 @@
 package models.cartes;
 
+import java.util.ArrayList;
+
 public abstract class Carte {
+    private int cout;
+    private ArrayList<TypeDeCarte> type;
+    private LocalisationDesCartes Localisation;
+
 }
